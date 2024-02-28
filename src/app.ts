77ef4baf295ed73b9cc1696a3504dc6b0a4e784c
BogdanGrynjuk <bgrynjuk@ukr.net>
2024-02-29 01:17:27 +0200

    configures tsconfig.json

---
+++ src/app.ts
@@ -0,0 +1,3 @@
+let some: string;
+
+some = 'hello world';
---
+++ src/button.ts
@@ -0,0 +1,7 @@
+const button = document.querySelector('#button-id')!;
+
+const handleClick = () => {
+  console.log("Hello world!")
+};
+
+button.addEventListener('click', handleClick);
